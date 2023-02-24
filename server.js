3047ba99149f74e0eeb2dee727768dca3d426f34
node <oherranmo@gmail.com>
2023-02-24 10:51:42 +0100

    Commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 
 port = 3080;
@@ -8,7 +9,26 @@ app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
 });
 
-app.get('/srv/node', (req,res) => {
-    const dades = { id: 1, nom: "Ian"}
-    res.json(dades);
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./book-net-eb5c1-firebase-adminsdk-atp4r-064fe385bd.json");
+const {getFirestore} = require("firebase-admin/firestore");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
 });
+const db = getFirestore();
+dbConnection();
+async function dbConnection(){
+    const conn = db.collection("book-net").doc("clients");
+    const doc = await conn.get();
+    if (!doc.exists){
+        console.log("El document no existeix!")
+    }else{
+        app.get('/api/firebase',(req,res)=>{
+            const document = doc.data();
+            res.json(document);
+        })
+    }
+}
