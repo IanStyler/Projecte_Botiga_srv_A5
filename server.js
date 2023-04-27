c27e730f4b8a36747c0ba5add9cf2ff9e957b01b
IanStyler <ian.soles@institutvidreres.cat>
2023-04-27 08:41:31 +0200

    Mysql

--- server.js
+++ server.js
@@ -5,10 +5,24 @@ const {FieldValue} = require('firebase-admin/firestore')
 app.use(cors());
 app.use(express.json());
 
+const mysql = require('mysql');
 port = 3080;
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
 });
+
+//connectar bd relacional Mysql____
+const connectionMysql = mysql.createConnection({
+    host: 'localhost', //Canviar per ip del servidor un cop estigui el node alla?
+    user: 'root',
+    password: 'admin',
+    database: 'projecta_botiga'
+});
+connectionMysql.connect((err)=>{
+    if (err) throw err;
+    console.log('Connectat!');
+});
+//______________________________________
 
 
 var admin = require("firebase-admin");
@@ -21,7 +35,7 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 const db = getFirestore();
-    dbConnection();
+dbConnection();
 
 async function dbConnection(){
     const conn = db.collection("book-net").doc("clients");
@@ -29,14 +43,14 @@ async function dbConnection(){
     if (!doc.exists){
         console.log("El document no existeix!")
     }else{
-            app.get('/api/firebase',async (req, res) => {
+        app.get('/api/firebase',async (req, res) => {
 
-                const conn = db.collection("book-net").doc("clients");
-                const doc = await conn.get();
+            const conn = db.collection("book-net").doc("clients");
+            const doc = await conn.get();
 
-                const document = doc.data();
-                res.json(document);
-            })
+            const document = doc.data();
+            res.json(document);
+        })
 
     }
 }
@@ -68,5 +82,16 @@ app.post('/datausersdelete',(req, res) => {
             Correu: req.body.Correu,
             Nom: req.body.Nom,
             Telèfon: req.body.Telèfon})
+    })
+})
+
+// Projecte Botiga A5
+
+
+//Prova de connexio a bd mysql
+app.get('/provaSelect',(req,res)=>{
+    connectionMysql.query("SELECT * FROM projecta_botiga.productes",(err,rows)=>{
+        if(err) throw err;
+        res.json(rows);
     })
 })
